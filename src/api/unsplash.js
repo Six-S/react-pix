6fd00c94571289e85fd9f6fc859281614b508e70
Six-S <brenlink@gmail.com>
2019-09-09 23:31:36 -0600

    Update README

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID bef52475429f3a15aef619e1032c51e63fa5bf2b68e3b0bf5f970705db3622bb'
+        Authorization: 'Client-ID [REDACTED]'
     }
 });
